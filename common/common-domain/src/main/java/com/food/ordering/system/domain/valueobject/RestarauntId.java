0b63f269aa984b113b75f0a9f80a551972f4daa8
IlyaNSV <pw2013pw2013@mail.ru>
2025-08-25 23:28:46 +0300

    Commit after lesson 12.

---
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/RestarauntId.java
@@ -0,0 +1,9 @@
+package com.food.ordering.system.domain.valueobject;
+
+import java.util.UUID;
+
+public class RestarauntId extends BaseId <UUID> {
+    public RestarauntId(UUID value) {
+        super(value);
+    }
+}
